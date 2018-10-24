97a71522c02574ba868c284fdf3567c4df953cc2
canhvedepzai <tphan2883@gmail.com>
2018-10-25 01:25:56 +0700

    Added Account Info Page

--- src/main/resources/static/dist/js/customscript.js
+++ src/main/resources/static/dist/js/customscript.js
@@ -40,4 +40,19 @@ $('.carousel-no-auto').carousel({
 });
 //End Custom Carousel
 
-
+//Image Preview
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+            $('#imagePreview').hide();
+            $('#imagePreview').fadeIn(650);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+$("#imageUpload").change(function() {
+    readURL(this);
+});
+//Image Preview
