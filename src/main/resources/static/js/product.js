718e872d50b4302c167c735748cf070a4fe227a5
hieplk97 <37424865+hieplk97@users.noreply.github.com>
2018-10-25 22:45:01 +0700

    Product Ajax code

--- src/main/resources/static/js/product.js
+++ src/main/resources/static/js/product.js
@@ -1,5 +1,15 @@
 $(function () {
+
+    const frontendServer = 'http://localhost:8084';
+    const backendServer = 'http://localhost:8080';
+
     $(document).read(function () {
-        ${}
+        $.ajax({
+            url: backendServer+"/api/product/4"
+        }).then(function (data) {
+            $('show-product-name').append(data.name);
+            $('show-product-price').append(data.description);
+            $('').append(data.price);
+        })
     })
 })
