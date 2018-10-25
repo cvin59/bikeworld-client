b6df6fc43c6bd6121ee6ae219d048a2a37b9db9e
hieplk97 <37424865+hieplk97@users.noreply.github.com>
2018-10-26 01:45:31 +0700

    tradepostdetail

--- src/main/resources/static/js/product.js
+++ src/main/resources/static/js/product.js
@@ -2,19 +2,16 @@ const frontendServer = 'http://localhost:8084';
 const backendServer = 'http://localhost:8080';
 
 function getProductById() {
-    let id = $("#id").val()
-    console.log(id);
-    if (id != null) {
-        $.ajax({
-            type: "GET",
-            url: backendServer + "api/product/" + id,
-            dataType: 'json',
-            success: function (response) {
-                $('#result').html("");
-                var product=JSON.parse(response){
+    $.ajax({
+        type: "GET",
+        url: backendServer + "api/product/" + id,
+        dataType: 'json',
+        success: function (response) {
+            $('#result').html("");
+            var product = JSON.parse(response)
+            document.getElementById("show-product-name").innerHTML = product.name;
 
-                }
-            }
-        })
-    }
+        }
+    })
+
 }
