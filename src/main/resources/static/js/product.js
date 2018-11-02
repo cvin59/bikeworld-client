12faf7ffe2c54a8ce886934717e6795e9d98339f
hieplk97 <37424865+hieplk97@users.noreply.github.com>
2018-11-03 00:21:31 +0700

    Paging on Client

--- src/main/resources/static/js/product.js
+++ src/main/resources/static/js/product.js
@@ -2,6 +2,12 @@ const backendServer = 'http://localhost:8080';
 
 var imgSeq = 0;
 var uploadImgList = [];
+var deleteImgList = [4, 5, 6];
+
+var productListPage = 1;
+var productListTotalPage;
+var productListSize = 5;
+
 
 $(function () {
     CKEDITOR.replace('inputProductDescription');
@@ -46,75 +52,72 @@ $(function () {
     }));
 
 
-    var filesUpload = document.getElementById("files-upload"),
-        fileList = document.getElementById("file-list");
+});
 
+function uploadFile(file, filesUpload, fileList) {
+    var tr = document.createElement("tr"),
+        nameTd = document.createElement("td"), sizeTd = document.createElement("td"),
+        imgTd = document.createElement("td"), btnTd = document.createElement("td"),
+        img = document.createElement("img"),
+        reader;
 
-    function uploadFile(file) {
-        var tr = document.createElement("tr"),
-            nameTd = document.createElement("td"), sizeTd = document.createElement("td"),
-            imgTd = document.createElement("td"), btnTd = document.createElement("td"),
-            img = document.createElement("img"),
-            reader;
+    if (typeof FileReader !== "undefined" && (/image/i).test(file.type)) {
+        img = document.createElement("img");
+        img.width = 200;
+        img.height = 100;
+        img.border = 1;
 
-        if (typeof FileReader !== "undefined" && (/image/i).test(file.type)) {
-            img = document.createElement("img");
-            img.width = 200;
-            img.height = 100;
-            img.border = 1;
+        tr.appendChild(imgTd);
+        imgTd.appendChild(img);
 
-            tr.appendChild(imgTd);
-            imgTd.appendChild(img);
+        reader = new FileReader();
 
-            reader = new FileReader();
+        reader.onload = (function (theImg) {
 
-            reader.onload = (function (theImg) {
-
-                return function (evt) {
-                    theImg.src = evt.target.result;
-                };
-            }
-            (img));
-            reader.readAsDataURL(file);
+            return function (evt) {
+                theImg.src = evt.target.result;
+            };
         }
+        (img));
+        reader.readAsDataURL(file);
+    }
 
 // Present file info and append it to the list of files
-        nameTd.innerHTML = file.name;
-        sizeTd.innerHTML = parseInt(file.size / 1024, 10) + " Kb";
+    nameTd.innerHTML = file.name;
+    sizeTd.innerHTML = parseInt(file.size / 1024, 10) + " Kb";
 
 
-        btnTd.innerHTML = "<button class=\"btn-outline-danger\" type=\"button\" onclick=" + "deleteImg(this," + imgSeq + ')' + ">\n" +
-            "<span>Delete</span>\n" +
-            "</button>\n";
+    btnTd.innerHTML = "<button class=\"btn-outline-danger\" type=\"button\" onclick=" + "deleteImg(this," + imgSeq + ')' + ">\n" +
+        "<span>Delete</span>\n" +
+        "</button>\n";
 
-        //     fileInfo += "<p><strong>Type:</strong> " + file.type + "</p>";
+    //     fileInfo += "<p><strong>Type:</strong> " + file.type + "</p>";
 
-        tr.appendChild(nameTd);
-        tr.appendChild(sizeTd);
-        tr.appendChild(btnTd);
+    tr.appendChild(nameTd);
+    tr.appendChild(sizeTd);
+    tr.appendChild(btnTd);
 
-        fileList.appendChild(tr);
+    fileList.appendChild(tr);
 
-    }
+}
 
-    function traverseFiles(files) {
-        if (typeof files !== "undefined") {
-            for (var i = 0, l = files.length; i < l; i++) {
-                uploadFile(files[i]);
-                uploadImgList.push(files[i]);
-                imgSeq++;
-            }
-        }
-        else {
-            fileList.innerHTML = "No support for the File API in this web browser";
+function traverseFiles(files, filesUpload, fileList) {
+    if (typeof files !== "undefined") {
+        for (var i = 0, l = files.length; i < l; i++) {
+            uploadFile(files[i], filesUpload, fileList);
+            uploadImgList.push(files[i]);
+            imgSeq++;
         }
     }
+    else {
+        fileList.innerHTML = "No support for the File API in this web browser";
+    }
+}
 
-    filesUpload.addEventListener("change", function () {
-        traverseFiles(this.files);
-    }, false);
-
-
+$('#create-files-upload').change(function () {
+    var filesUpload = document.getElementById("create-files-upload");
+    var fileList = document.getElementById("create-file-list");
+    traverseFiles(this.files, filesUpload, fileList);
 });
 
 $("#btnClear").click(function () {
@@ -159,6 +162,7 @@ $('#create-product-form').submit(function () {
             description: CKEDITOR.instances['inputProductDescription'].getData(),
             price: document.getElementById('inputProductPrice').value,
             quantity: document.getElementById('inputProductQuantity').value,
+            address: document.getElementById('inputProductAddress').value,
             seller: localStorage.getItem("username"),
             category: cate.options[cate.selectedIndex].value,
             brand: brand.options[brand.selectedIndex].value,
@@ -166,6 +170,7 @@ $('#create-product-form').submit(function () {
     var objectDataString = JSON.stringify(objectData);
 
     var formData = new FormData();
+
     formData.append('productModelString', objectDataString);
     for (i = 0; i < uploadImgList.length; i++) {
         if (uploadImgList[i] != null) {
@@ -181,25 +186,73 @@ $('#create-product-form').submit(function () {
         data: formData,
         contentType: false,
         processData: false,
-        success: function (data) {
+        success: function () {
             alert('Success');
         },
         error: function (e) {
             console.log(e);
-            alert("error")
+            alert("error:" + e)
+        }
+    });
+});
+
+$('#edit-product-form').submit(function () {
+
+    var objectData =
+        {
+            id: document.getElementById("editProductId").value,
+            name: document.getElementById('editProductName').value,
+            description: CKEDITOR.instances['editProductDescription'].getData(),
+            price: document.getElementById('editProductPrice').value,
+            quantity: document.getElementById('editProductQuantity').value,
+            address: document.getElementById('editProductAddress').value,
+        };
+    var objectDataString = JSON.stringify(objectData);
+    var formData = new FormData();
+    formData.append("productModelString", objectDataString);
+
+    var deleteImgJson = JSON.stringify(deleteImgList);
+    alert(deleteImgJson);
+
+    formData.append("deleteImgList", deleteImgJson);
+
+    for (i = 0; i < uploadImgList.length; i++) {
+        if (uploadImgList[i] != null) {
+            formData.append('images', uploadImgList[i]);
+        }
+    }
+    $.ajax({
+        type: "PUT",
+        url: backendServer + "/api/product",
+        dataType: "json",
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function () {
+            alert("Success");
+        }, error: function (e) {
+            alert("Error: " + e);
         }
     });
 });
 
 
-$('#product-list-link').one('click', function () {
+// Show Product List
+$('#product-list-link').one("click", showProductList());
+
+function showProductList() {
     var seller = localStorage.getItem("username");
+
+    var sort;
+
     $.ajax({
-        url: backendServer + "/api/product/seller/" + seller,
+        url: backendServer + "/api/product/seller/" + seller + "?page=" + productListPage + "&size=" + productListSize,
         dataType: 'json',
         type: 'GET',
         success: function (res) {
-            var productList = res.data;
+            var productList = res.data.viewModels;
+            productListPage = res.data.currentPage;
+            productListTotalPage = res.data.totalPage;
 
             if (productList != null) {
                 for (i = 0; i < productList.length; i++) {
@@ -293,6 +346,7 @@ $('#product-list-link').one('click', function () {
                     $("#show-product-stars-" + i).html(stars);
 
                     showStatus(productList[i].productInfo.statusId, i, $("#show-product-status-" + i));
+                    productListPagination(productListTotalPage, productListPage);
                 }
 
 
@@ -302,7 +356,7 @@ $('#product-list-link').one('click', function () {
             alert(e);
         }
     })
-});
+};
 
 function showStatus(stat, i, location) {
     var statusId = stat.id;
@@ -352,13 +406,62 @@ function showEditPage(seq) {
     $("#editProductAddress").val(product.productInfo.address);
     $("#editProductPrice").val(product.productInfo.price);
     $("#editProductQuantity").val(product.productInfo.quantity);
-    $("#editProductDescription").val(product.productInfo.description);
-    CKEDITOR.instances.editor1.setData(t);
+    CKEDITOR.instances.editProductDescription.setData(product.productInfo.description);
+
+    var images = product.ProductImg;
+    var imageId = product.ProductImgId;
+    var fileList = document.getElementById("edit-file-list");
+    for (i = 0; i < images.length; i++) {
+        showEditImage(images[i], imageId[i], fileList);
+    }
 }
+
+function showEditImage(image, imageId, fileList) {
+    var tr = document.createElement("tr"),
+        nameTd = document.createElement("td"), sizeTd = document.createElement("td"),
+        imgTd = document.createElement("td"), btnTd = document.createElement("td"),
+        img = document.createElement("img");
+
+    img.width = 200;
+    img.height = 100;
+    img.border = 1;
+    img.src = backendServer + image;
+
+    tr.appendChild(imgTd);
+    imgTd.appendChild(img);
+
+// Present file info and append it to the list of files
+    nameTd.innerHTML = image.split('/')[2];
+
+    btnTd.innerHTML = "<button class=\"btn-outline-danger\" type=\"button\" onclick='deleteOriginImg(this" + ',' + imageId + ")'\n" +
+        "<span>Delete</span>\n" +
+        "</button>\n";
+
+    //     fileInfo += "<p><strong>Type:</strong> " + file.type + "</p>";
+
+    tr.appendChild(nameTd);
+    tr.appendChild(sizeTd);
+    tr.appendChild(btnTd);
+
+    fileList.appendChild(tr);
+}
+
+function deleteOriginImg(btn, id) {
+    $(btn).closest('tr').remove();
+    deleteImgList.push(id);
+}
+
+$("#edit-files-upload").change(function () {
+    var edtFilesUpload = document.getElementById("edit-files-upload");
+    var edtFileList = document.getElementById("edit-file-list");
+    traverseFiles(this.files, edtFilesUpload, edtFileList);
+})
 
 function showDetailPage(seq) {
     var product = JSON.parse(localStorage.getItem('sellProduct-' + seq));
     $("#detailProductName").html(product.productInfo.name);
+
+    $("#detailProductAvatar").attr("src", backendServer + product.ProductImg[0]);
 
     var rate = product.productInfo.totalRatePoint;
     var rater = product.productInfo.totalRates;
@@ -375,3 +478,119 @@ function showDetailPage(seq) {
     showStars(rate, rater, $("#detailProductRate"));
     showStatus(product.productInfo.statusId, seq, $("#detailProductStatus"));
 }
+
+
+function productListPagination(totalPage, currentPage) {
+
+    if (currentPage < 2) {
+        document.getElementById("productList-first-page").className = "page-item disabled";
+        document.getElementById("productList-previous-page").className = "page-item disabled";
+    } else {
+        document.getElementById("productList-first-page").className = "page-item";
+        document.getElementById("productList-previous-page").className = "page-item";
+    }
+
+    if (currentPage == totalPage) {
+        document.getElementById("productList-last-page").className = "page-item disabled";
+        document.getElementById("productList-next-page").className = "page-item disabled";
+    } else {
+        document.getElementById("productList-last-page").className = "page-item";
+        document.getElementById("productList-next-page").className = "page-item";
+    }
+
+    switch (currentPage) {
+        case 1:
+            $("#productList-back2").css("display", "none");
+            $("#productList-back").css("display", "none");
+            break;
+        case 2:
+            $("#productList-back2").css("display", "none");
+            $("#productList-back").css("display", "block");
+            break;
+
+        case totalPage:
+            $("#productList-next").css("display", "none");
+            $("#productList-next2").css("display", "none");
+            break;
+        case totalPage - 1:
+            $("#productList-next").css("display", "none");
+            $("#productList-next2").css("display", "block");
+            break;
+        default:
+            $("#productList-back2").css("display", "block");
+            $("#productList-back").css("display", "block");
+            $("#productList-next2").css("display", "block");
+            $("#productList-next").css("display", "block");
+            break;
+    }
+    var back2 = currentPage - 2;
+    $("#productList-back2").html("  <a class=\"page-link\">" + back2 + "</a>");
+
+    var back = currentPage - 1;
+    $("#productList-back").html("  <a class=\"page-link\">" + back + "</a>");
+
+    $("#productList-current-page").html("  <a class=\"page-link\">" + currentPage + "</a>");
+
+    var next = currentPage + 1;
+    $("#productList-next").html("  <a class=\"page-link\">" + next + "</a>");
+
+    var next2 = currentPage + 2;
+    $("#productList-next2").html("  <a class=\"page-link\">" + next2 + "</a>");
+}
+
+
+$("#productList-next").click(function () {
+    productListPage += 1;
+    $("#show-product-list").html("");
+    showProductList();
+});
+
+$("#productList-next2").click(function () {
+    productListPage += 2;
+    $("#show-product-list").html("");
+    showProductList();
+});
+
+$("#productList-back").click(function () {
+    productListPage -= 1;
+    $("#show-product-list").html("");
+    showProductList();
+});
+
+$("#productList-back2").click(function () {
+    productListPage -= 2;
+    $("#show-product-list").html("");
+    showProductList();
+});
+
+$("#productList-first-page").click(function () {
+    productListPage = 1;
+    $("#show-product-list").html("");
+    showProductList();
+});
+
+$("#productList-last-page").click(function () {
+    productListPage = productListTotalPage;
+    $("#show-product-list").html("");
+    showProductList();
+});
+
+$("#productList-previous-page").click(function () {
+    if (productListPage - productListTotalPage < 5) {
+        productListPage = 1;
+    } else {
+        productListPage -= 5;
+    }
+    $("#show-product-list").html("");
+    showProductList();
+});
+
+$("#productList-next-page").click(function () {
+    if (productListTotalPage - productListPage < 5) {
+        productListPage = productListTotalPage;
+    } else {
+        productListPage -= 5;
+    }
+    $("#show-product-list").html("");
+    showProductList();
+});
