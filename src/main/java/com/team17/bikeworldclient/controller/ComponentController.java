11805a406311252a22e2d9aa9abedbe01c4075e4
atgmep <whalebun@gmail.com>
2018-11-12 21:04:03 +0700

    1
    1

--- src/main/java/com/team17/bikeworldclient/controller/ComponentController.java
+++ src/main/java/com/team17/bikeworldclient/controller/ComponentController.java
@@ -20,21 +20,21 @@ public class ComponentController {
 
 
 
-    @GetMapping("/category/{id}")
-    public ModelAndView viewProductCategory(@PathVariable Integer id) {
-        ModelAndView mav = new ModelAndView("");
-        mav.addObject("id", id);
-        return new ModelAndView("");
-    }
+//    @GetMapping("/category/{id}")
+//    public ModelAndView viewProductCategory(@PathVariable Integer id) {
+//        ModelAndView mav = new ModelAndView("");
+//        mav.addObject("id", id);
+//        return new ModelAndView("");
+//    }
+//
+//    @GetMapping("/search")
+//    public ModelAndView searchEvent(@RequestParam("searchValue") String searchValue){
+//        ModelAndView mav = new ModelAndView("trading-search");
+//        mav.addObject("searchValue", searchValue);
+//        return mav;
+//    }
 
-    @GetMapping("/search")
-    public ModelAndView searchEvent(@RequestParam("searchValue") String searchValue){
-        ModelAndView mav = new ModelAndView("trading-search");
-        mav.addObject("searchValue", searchValue);
-        return mav;
-    }
-
-    @GetMapping("/list")
+    @GetMapping("")
     public ModelAndView viewProductList() {
         return new ModelAndView("component");
     }
